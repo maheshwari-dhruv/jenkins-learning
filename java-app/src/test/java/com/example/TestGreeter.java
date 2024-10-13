8cdca3412a6b91d7d95bbfe220211ef71698075b
maheshwari-dhruv <mrdm026@gmail.com>
2024-10-13 13:01:11 +0000

    added failed test case

--- java-app/src/test/java/com/example/TestGreeter.java
+++ java-app/src/test/java/com/example/TestGreeter.java
@@ -26,7 +26,11 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone), containsString(someone));
   }
-  
+
+  @Test
+  public void failedTestCase() {
+    assertTrue(false);
+  }
 
   @Test
   public void greetShouldIncludeGreetingPhrase() {
